2f6d165ec545cf50ef274d971e41139de5bc561d
Tendo33 <sjf1998112@gamil.com>
2024-07-19 14:59:09 +0800

    tiny fix

--- pdf_parser/delete_images.py
+++ pdf_parser/delete_images.py
@@ -34,8 +34,8 @@ def move_directories(src_dir, dst_dir):
 
 if __name__ == '__main__':
 
-    folder_path = r'/home/sunjinf/github_projet/nature_data/origin_pdf_second'
-    target_dir = r'/home/sunjinf/github_projet/nature_data/out_second'
+    folder_path = r'/home/sunjinf/github_projet/nature_data/origin_pdf_third'
+    target_dir = r'/home/sunjinf/github_projet/nature_data/out_third'
 
     delete_images_in_subfolders(folder_path)
     move_directories(folder_path, target_dir)
