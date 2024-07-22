1402df5920621a8efd7e649917f24e72412a4b90
Tendo33 <sjf1998112@gmail.com>
2024-07-22 11:46:29 +0800

    tiny fix

--- pdf_parser/extract_text_from_mk_vllm.py
+++ pdf_parser/extract_text_from_mk_vllm.py
@@ -27,7 +27,8 @@ SYSTEM_PROMPT = '''
 - 错字识别与修复
 
 ## Constraints
-- 忽略图片和表格内容信息。
+- 忽略图片和信息，不要输出跟图片相关的信息。
+- 用文字总结表格，不要超过200个字。
 - 不要编纂内容，只输出跟原文相关的内容。
 - 按照原文意思提取出有用的信息，如果句子不完整或者不通顺，你要使其变得完整。
 - 返回的内容中的正文不要带markdown格式，只有标题可以带markdown格式。
@@ -36,7 +37,7 @@ SYSTEM_PROMPT = '''
 
 ## Workflow
 1. 用户提供Markdown文件内容。
-2. 提取文件中的纯文字和信息。
+2. 提取文件中的文字和信息。
 3. 识别并修复其中的中文错字漏字使其变为通顺的句子。
 4. 只返回修复后的纯文字内容。
 '''
@@ -58,7 +59,7 @@ def construct_query(content: str) -> str:
     return prompt
 
 
-def split_into_chunks(text: str, chunk_size: int = 4000) -> list:
+def split_into_chunks(text: str, chunk_size: int = 6000) -> list:
     return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
 
 
@@ -108,7 +109,7 @@ if __name__ == "__main__":
 
     # Sampling参数
     sampling_params = SamplingParams(
-        temperature=1, top_p=0.95)
+        temperature=1, top_p=0.95, max_tokens=8192)
     llm = LLM(model=MODEL_PATH, dtype="auto", tensor_parallel_size=1,
               tokenizer_mode="auto", gpu_memory_utilization=0.95, enforce_eager=True)
 
